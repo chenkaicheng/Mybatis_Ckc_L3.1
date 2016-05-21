642ec6a6f36bf2e2df296057d2df85ebd2936bdd
chenkaicheng <ChenKaiCheng520@163.com>
2016-05-21 13:49:48 +0800

    2

---
+++ src/com/ckc/mybatis/l3/test/Test.java
@@ -0,0 +1,14 @@
+package com.ckc.mybatis.l3.test;
+/**  
+* @author:Ckc 
+* @project:Mybatis_Ckc_L3.1
+* @Title: Test
+* @Package com.ckc.mybatis.l3.test 
+* @Description: (用一句话描述这个类的作用) 
+* @date 2016年5月21日 下午1:49:31  
+*/
+public class Test
+{
+
+}
+
